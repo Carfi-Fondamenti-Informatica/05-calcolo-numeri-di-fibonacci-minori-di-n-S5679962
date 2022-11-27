388f98c0a22415c3e4d5d241efd50006ee17d0d7
Gregorio Minetto <117943888+S5679962@users.noreply.github.com>
2022-11-27 22:35:19 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 
 int main() {
+   int n, i, a, b, c;
+    cin >> n;
+    a=1;
+    b=1;
+    cout << a << endl << b << endl;
+        for (i=0; i<=n; i=i+a){
+            c=a+b;
+            a=b;
+            b=c;
+            cout << c << endl;
+        }
+    }
    return 0;
 }
